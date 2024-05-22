fda4ae5ed5ccacbf6dac8d56bfb5fa89614ebc3b
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-05-22 11:07:38 -0300

    Search Form

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./SearchForm";
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
@@ -9,6 +10,7 @@ export function Transactions() {
             <Summary />
 
             <TransactionContainer>
+                <SearchForm />
                 <TransactionTable>
                     <tbody>
                         <tr>
