f472c06690c945416937de39352d5096e28b168b
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-05-30 15:22:07 -0300

    Button radio

--- src/components/NewTrasactionModal/index.tsx
+++ src/components/NewTrasactionModal/index.tsx
@@ -20,11 +20,11 @@ export function NewTransactionModal() {
                     <input type="text" placeholder="Category" required />
 
                     <TransactionType>
-                        <TransactionTypeButton variant="income">
+                        <TransactionTypeButton variant="income" value="income">
                             <ArrowCircleUp size={24} /> 
                             Entry
                         </TransactionTypeButton>
-                        <TransactionTypeButton variant="outcome">
+                        <TransactionTypeButton variant="outcome" value="outcome">
                             <ArrowCircleDown size={24} /> 
                             Exit
                         </TransactionTypeButton>
