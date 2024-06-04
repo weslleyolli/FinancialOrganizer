f9dbb2dbd72d158888cfdf5f1246074ce7fcf8bd
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-06-04 16:10:00 -0300

    Creating transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
 import { Transactions } from "./pages/transactions"
+import { TransactionsProvider } from "./context/TransactionsContext"
+
 
 
 export function App() {
-  return(
+  return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+
     </ThemeProvider>
   )
 }
--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./SearchForm";
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
+import { TransactionsContext } from "../../context/TransactionsContext";
 
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
-}
+
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3333/transactions')
-        const data = await response.json()
-
-        setTransactions(data)
-    }
-
-
-
-    useEffect(() => {
-        loadTransactions()
-    }, [])
-
-
+    const { transactions } = useContext(TransactionsContext)
     return (
         <div>
             <Header />
